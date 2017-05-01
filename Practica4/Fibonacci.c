7bdbb1b4ef2257d5efe74a594c2bafee02053d5e
Luis Emmanuel Maya Rocha <maya.rocha.emmanuel@gmail.com>
2017-05-01 18:00:08 -0500

    Incluì màs comentarios

--- Practica4/Fibonacci.c
+++ Practica4/Fibonacci.c
@@ -1,7 +1,8 @@
+// Biblioteca del programa fibonacci. Equipo: 932 
 #include "Fibonacci.h"
 
 /*
-Instrucciones: Calcula la posicion (numero 'n') de la serie fibonacci que se indica.
+Instrucciones: Calcula la posiciòn (numero 'n') de la serie fibonacci que se indica.
 Recibe: Posicion o numero en la serie. 
 Retorna: Entero, el valor encontrado en esa posicion. */
 double
@@ -9,7 +10,7 @@ fibonacci (int n)
 {
 /*La funcion se encuentra sin optimizar, Si el numero de la serie es menor a 2, se imprime, terminando la recursividad */
   if (n < 2)
-    return n;
+    return n;  //Si el numero ingresado "n" es menor a 2, se imprime dicho numero, si no, se realiza la suma, que por definiciòn, pide la serie fibonacci
   else
     return fibonacci (n - 1) + fibonacci (n - 2);
 }
