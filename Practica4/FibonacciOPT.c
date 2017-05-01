05cde50451d0b82622bfa5540d67a6b33513a69c
Luis Emmanuel Maya Rocha <maya.rocha.emmanuel@gmail.com>
2017-05-01 18:07:24 -0500

    Update FibonacciOPT.c

--- Practica4/FibonacciOPT.c
+++ Practica4/FibonacciOPT.c
@@ -1,3 +1,4 @@
+// Biblioteca optimizada, reduciendo hasta el 35% aproximadamente del tiempo para obtener resultados.
 #include "Fibonacci.h"
 
 /*
@@ -10,13 +11,13 @@ fibonacci (int n)
 /*La funcion se encuentra optimizada para calcular numeros grandes en un poco menos de tiempo. */
   switch (n)
     {
-    case 2:
+    case 2:  //En este caso, tomamos en cuenta que el valor de n=2 tambièn es "1", por lo que lo escribimos, reduciendo llamadas recursivas.
       return 1;
     case 1:
       return 1;
     case 0:
       return 0;
     default:
-      return fibonacci (n - 1) + fibonacci (n - 2);
+      return fibonacci (n - 1) + fibonacci (n - 2);  //A continuaciòn, se da el mismo comportamiento del fibonacci recursivo "normal".
     }
 }
