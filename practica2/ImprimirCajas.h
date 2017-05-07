95e25a9a71f375dacd4124699b5aadee1d8a5d64
Luis Emmanuel Maya Rocha <maya.rocha.emmanuel@gmail.com>
2017-05-07 11:37:10 -0500

    Create ImprimirCajas.h

---
+++ practica2/ImprimirCajas.h
@@ -0,0 +1,8 @@
+/*Cabecera del programa de simulaciòn 1 (Pràctica 2)
+Autor: Equipo 932
+Año 2017 */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void Imprimir_en_pantalla (cola cajas[10], int i, int tiempos[11], int tiempo_total, int atendidos, elemento e, char nom_merc[100]); //Funciòn que imprimira el estado de las colas, con atractivo visual para dar a entender que es un centro comercial.
