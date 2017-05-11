051362f371646aafe27d6adc4c42e324f21d5f05
Luis Emmanuel Maya Rocha <maya.rocha.emmanuel@gmail.com>
2017-05-10 23:18:30 -0500

    Ignoren

--- Practica4/Fibonacci.c
+++ Practica4/Fibonacci.c
@@ -12,5 +12,8 @@ fibonacci (int n)
   if (n < 2)
     return n;  //Si el numero ingresado "n" es menor a 2, se imprime dicho numero, si no, se realiza la suma, que por definiciòn, pide la serie fibonacci
   else
-    return fibonacci (n - 1) + fibonacci (n - 2);
+    return fibonacci (n - 1) + fibonacci (n - 2); 
+  /*Ejemplo: Si n=3, hace la suma de los valores de la serie, en n=2 y n=1, para saber estos valores, vuelve a realizar el mismo càlculo (recursividad) para n=2
+  y n=1, asì, para n=1 (caso base), regresa 1 y se le suma 1 (valor resultante para la posicion n=2, ya que se sumo los valores de n=0 y n=1, es decir,
+  0+1). Finalmente, el resultado serà 1+1 = 2*/
 }
