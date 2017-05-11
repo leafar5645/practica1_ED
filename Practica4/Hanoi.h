5d359f10ba2dc4036985a6d960842857597a02b1
Luis Emmanuel Maya Rocha <maya.rocha.emmanuel@gmail.com>
2017-05-10 23:30:47 -0500

    Ignoren

--- Practica4/Hanoi.h
+++ Practica4/Hanoi.h
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> //Incluimos cabecera stdio para usar printfs y scanfs
+#include <stdlib.h> 
 
-void Hanoi (int ndiscos, int A, int C, int B, int repetHan);
-void pintar (int ndiscos, int A, int C, int B, int RepetHan);
+void Hanoi (int ndiscos, int A, int C, int B, int repetHan); //Funciòn Hanoi, realiza càlculos recursivos.
+void pintar (int ndiscos, int A, int C, int B, int RepetHan); //Funciòn pintar, pinta el disco a mover. o como irìa quedando la torre.
