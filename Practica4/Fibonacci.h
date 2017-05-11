1c031f7286b38499bec5bf485c551d3afa9705b4
Luis Emmanuel Maya Rocha <maya.rocha.emmanuel@gmail.com>
2017-05-10 23:09:50 -0500

    Ignoren

--- Practica4/Fibonacci.h
+++ Practica4/Fibonacci.h
@@ -1,3 +1,3 @@
-#include <stdio.h>
+#include <stdio.h> //Incluimos biblioteca estàndar
 
-double fibonacci (int n);
+double fibonacci (int n); //Funciòn que realiza el càlculo, de forma recursiva, de la serie fibonacci, se utiliza double por la cantidad de digitos que pueden aparecer.
