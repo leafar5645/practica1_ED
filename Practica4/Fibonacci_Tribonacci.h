3de8f536c6dc854289f15cec9de44f6f27f275da
Luis Emmanuel Maya Rocha <maya.rocha.emmanuel@gmail.com>
2017-05-15 11:47:32 -0500

    Lista

--- Practica4/Fibonacci_Tribonacci.h
+++ Practica4/Fibonacci_Tribonacci.h
@@ -1,4 +1,4 @@
-/* Título: fibonacci.h 
+/* Título: Fibonacci_Tribonacci.h 
    Descripción: Cabecera que incluye las bibliotecas predefinidas y define las funciones. 
    Autor: Equipo 932            Fecha: 11 de Mayo de 2017
    Versión: 1.3
@@ -6,3 +6,4 @@
 #include <stdio.h> //Incluimos biblioteca estàndar
 
 double fibonacci (int n); //Funciòn que realiza el càlculo, de forma recursiva, de la serie fibonacci, se utiliza double por la cantidad de digitos que pueden aparecer.
+double tribonacci (int n); //Funciòn que realiza el càlculo, de forma recursiva, de la serie fibonacci, se utiliza double.
