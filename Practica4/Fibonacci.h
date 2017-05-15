fc9575e6939c2b0472de359acb33aac46e9a88d4
Luis Emmanuel Maya Rocha <maya.rocha.emmanuel@gmail.com>
2017-05-15 10:44:26 -0500

    Lista para entregar.

--- Practica4/Fibonacci.h
+++ Practica4/Fibonacci.h
@@ -1,3 +1,8 @@
+/* Título: fibonacci.h 
+   Descripción: Cabecera que incluye las bibliotecas predefinidas y define las funciones. 
+   Autor: Equipo 932            Fecha: 11 de Mayo de 2017
+   Versión: 1.3
+*/
 #include <stdio.h> //Incluimos biblioteca estàndar
 
 double fibonacci (int n); //Funciòn que realiza el càlculo, de forma recursiva, de la serie fibonacci, se utiliza double por la cantidad de digitos que pueden aparecer.
