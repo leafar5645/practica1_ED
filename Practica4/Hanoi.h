149edc85a3f6a4e9c3c7bbce41dbed72dbab38bd
Luis Emmanuel Maya Rocha <maya.rocha.emmanuel@gmail.com>
2017-05-15 12:15:31 -0500

    Lista

--- Practica4/Hanoi.h
+++ Practica4/Hanoi.h
@@ -1,5 +1,9 @@
 /* Título: Hanoi.h 
    Descripción: Cabecera que incluye las bibliotecas predefinidas y define las funciones.
+   -Recibe: cada una recibe el “tamaño del disco en curso” (ndiscos), el palo inicial, destino y auxiliar y la variable repetHan, aunque en pintar solo se necesitaran los palos, al llamarla al final del programa principal.
+   -Retorna: Solo hacen operaciones, no regresan algo como tal.
+
+   
     Autor: Equipo 932            Fecha: 11 de Mayo de 2017
     Versión: 1.0
 */
