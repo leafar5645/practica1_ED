5d4a0db53fa575c859411af192883388779ad5fe
Luis Emmanuel Maya Rocha <maya.rocha.emmanuel@gmail.com>
2017-05-22 19:09:18 -0500

    Listo

--- Practica4/Hanoi.h
+++ Practica4/Hanoi.h
@@ -2,10 +2,13 @@
    Descripción: Cabecera que incluye las bibliotecas predefinidas y define las funciones.
    -Recibe: cada una recibe el “tamaño del disco en curso” (ndiscos), el palo inicial, destino y auxiliar y la variable repetHan, aunque en pintar solo se necesitaran los palos, al llamarla al final del programa principal.
    -Retorna: Solo hacen operaciones, no regresan algo como tal.
-
    
-    Autor: Equipo 932            Fecha: 11 de Mayo de 2017
-    Versión: 1.0
+    Autor: Equipo 932
+    Integrantes:
+       - Hernández Ruiz Rafael
+	    - Maya Rocha Luis Emmanuel
+	    - Rivas Rojas Arturo
+    Fecha: 11 de Mayo de 2017               Versión: 1.1
 */
 #include <stdio.h> //Incluimos cabecera stdio para usar printfs y scanfs
 #include <stdlib.h> 
