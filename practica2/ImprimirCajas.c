d4c72b6bfa71ed611276e0da9b3311b1f1f9df7c
Luis Emmanuel Maya Rocha <maya.rocha.emmanuel@gmail.com>
2017-05-30 18:57:14 -0500

    Listo

--- practica2/ImprimirCajas.c
+++ practica2/ImprimirCajas.c
@@ -1,56 +1,67 @@
-#include "ImprimirCajas.h"  
+/*  Título: ImprimirCajas.c
+    Autor: Equipo 932
+    Integrantes:
+            - Hernández Ruiz Rafael
+	    - Maya Rocha Luis Emmanuel
+	    - Rivas Rojas Arturo
+    Fecha: 1 de junio de 2017               Versión: 1.5
+*/
+#include "ImprimirCajas.h"
 
+/* Imprimir_en_pantalla, es la funciòn encargada de imprimir de manera visualmente atractiva, el estado de la simulaciòn en un momento determinado, imprimiendo los clientes formados en cada cola, indicando cual es atendido y que cajas estan en servicio.
+Recibe: El arreglo de colas (o cajas), una variable "i" para recorridos en ciclos, los tiempos de atenciòn de cada caja y tiempo de llegada de los clientes, el tiempo total del programa, la cantidad de clientes atendidos, el cliente en caja (e) y el nombre del supermercado.
+Retorna: No regresa nada como tal, solo imprime cosas.*/
 void Imprimir_en_pantalla (cola cajas[10], int i, int tiempos[11], int tiempo_total, int atendidos, elemento e, char nom_merc[100])
-{  //Funciòn que serà llamada cada vez que se quiera imprimir el estado de las colas del supermercado.
-      system ("/usr/bin/clear");		//Borra lo escrito en pantalla.
+{
+      system ("/usr/bin/clear");		//Borra la pantalla. Instruccion no perteneciente a ANSI C
 	      printf ("\n\t\t\t\t\tNombre del Supermercado: %s\t\t\t", nom_merc);	
       printf ("\n\t\tRecuerde que cerramos cuando 100 clientes sean atendidos y no haya clientes en espera.\n\n\t\t\t");
       printf ("  ");
-	  for (i = 0; i < 10; i++)		//Se imprimer las cajas, esten o no activas
+	  for (i = 0; i < 10; i++)		//Se imprimen las cajas, esten o no activas
       {
-	printf (" caja %d ", i + 1);                 
+	printf (" caja %d ", i + 1);   
       }
-      printf("\n\nTiempo de atencion:\t");      //Tambièn se imprimira el tiempo de atenciòn señalado para cada caja.
+      printf("\n\nTiempo de atencion:\t");  //Se imprime el tiempo de atenciòn escrito para cada caja
       for (i = 0; i < 10; i++)
 	{
-		if(tiempos[i]==0)              //Si no se ingreso un tiempo para la caja indicada, quiere decir que èsta, esta fuera de servicio
-		printf ("   --\t");            // Al estar fuera de servicio, se imprime una linea horizontal
+		if(tiempos[i]==0)   //Si la caja esta fuera de servicio, se imprime una linea en donde irìa el tiempo.
+		printf ("   --\t");
 		else
 	  printf ("   %d\t", tiempos[i]);
 	}
 
-      printf ("\n\n\n\nAtendido:\t\t");	       
+      printf ("\n\n\n\nAtendido:\t\t");	  //A continuaciòn se imprime una fila de atendidos en cada caja, es decir, el primer elemento (o cliente) de la cola
       for (i = 0; i < 10; i++)
       {
-      	// A continuacion imprimiremos para cada fila, los clientes formados en fila, sabiendo que habra si el tamaño de la cola -1 (el atendido), es mayor o igual al numero de cliente en la fila pedido.
-	if (Size (&cajas[i])-1 >= 0)	//Si no esta vacia, se imprime el elemento al frente de la cola, el cual se simula que es el que esta siendo atendido
+      	// A continuacion imprimiremos para cada fila, los clientes en fila, sabiendo que habra si el tamaño de la cola -1 (el atendido), es mayor o igual a lo que se pide.
+	if (Size (&cajas[i])-1 >= 0)	//Si no esta vacìa, se imprime el elemento al frente de la cola, el cual se simula que es el que esta siendo atendido
 	  {
-	    e = Front (&cajas[i]);      
-	    printf ("   %d\t", e.n);
+	    e = Front (&cajas[i]);  
+	    printf ("   %d\t", e.n); //Imprimiendo cliente atendido
 	  }
 	  else if(tiempos[i]==0)   //Si la caja esta fuera de servicio, se imprimira una "S" 
 	printf ("   S \t");
-	else			//Si no hay clientes formados en la posicion dicha (no hay ni un solo cliente en la fila, ni si quiera uno atendido), se imprimira una "N"
+	else			//Si no hay clientes formados, se imprimira una "N"
 	  printf ("   N\t");
       }
-            //Se imprimen a continuacion, los clientes formados, no atendidos aun
+            //Se imprimen a continuacion, los clientes formados, no atendidos aùn
 /* ------------------------------------------------------------------------ */
       printf ("\n\n\nPrimero en fila:\t");	
-      for (i = 0; i < 10; i++)     //Estos bloques for son los encargados de recorrer "el arreglo" de colas, con ello podremos imprimir "horizontalmente" lo pedido.
+      for (i = 0; i < 10; i++)
 	{
-	  if (Size (&cajas[i])-1 >= 1)   //Recordemos que le restamos 1 al tamaño pues no estamos contando al primer elemento en fila (cliente atendido)
+	  if (Size (&cajas[i])-1 >= 1) //Siempre verificandose que exista la cantidad de clientes necesaria para que haya un primer/segundo y tercer lugar en la cola de espera
 	    {
 	      e = Element (&cajas[i], 2);  //Se imprime el elemento en la posicion dada, de la caja ingresada (posicion del arreglo de cajas)
 	      printf ("   %d\t", e.n);
 	    }
-	  else if(tiempos[i]==0)    //De la misma forma que como se hizo con la fila horizontal del cliente atendido, si la caja esta fuera de servicio, se imprime "S", si no hay clientes, "N"
+	  else if(tiempos[i]==0) //Recordemos que si la caja esta fuera de servicio, se imprimirà "S" y si no hay clientes formados en el lugar indicado de la fila, se imprimirà una "N"
 	printf ("   S \t");
 	  else
 	    printf ("   N\t");
 	}
 
-      printf ("\n\nSegundo en fila:\t");   //Repetimos un procedimiento similar para imprimir (horizontalmente) los clientes formados en segundo lugar en cada fila
-      for (i = 0; i < 10; i++)	
+      printf ("\n\nSegundo en fila:\t"); //Repetimos el mismo procedimiento de impresiòn en pantalla, para los segundos y terceros en cada fila, cambiando el if correspondiente
+      for (i = 0; i < 10; i++)	 //Recordemos que el ciclo for recorre horizontalmente, lo que implica que se imprima el cliente de cada cola, o mejor dicho cada caja del supermercado
 	{
 	  if (Size (&cajas[i])-1 >= 2)
 	    {
@@ -63,10 +74,10 @@ void Imprimir_en_pantalla (cola cajas[10], int i, int tiempos[11], int tiempo_to
 	    printf ("   N\t");
 	}
 
-      printf ("\n\nTercero en fila:\t");  //.... y tambièn para los clientes en el tercer lugar.
-      for (i = 0; i < 10; i++)
+      printf ("\n\nTercero en fila:\t");  //Ahora se imprime el tercero de cada fila, si es que existe (si no, se imprime "N" o "S").
+      for (i = 0; i < 10; i++) 
 	{
-	  if (Size (&cajas[i]) -1 >= 3)
+	  if (Size (&cajas[i]) -1 >= 3) 
 	    {
 	      e = Element (&cajas[i], 4);
 	      printf ("   %d\t", e.n);
@@ -75,23 +86,23 @@ void Imprimir_en_pantalla (cola cajas[10], int i, int tiempos[11], int tiempo_to
 	printf ("   S \t");
 	  else
 	    printf ("   N\t");
-	}
+	}  //Hasta ahora, hemos imprimido los clientes atendidos y los clientes formados en 1ª, 2ª y 3ª lugar de cada cola por caja (si existian)
 
     /* ------------------------------------------------------------------------ */
     
-      printf ("\n\n\nTamano de fila:\t\t");	//Imprime el tamaño de la fila, el numero de clientes  no atendidos aun
+      printf ("\n\n\nTamano de fila:\t\t");	//Imprime el tamaño de la fila de clientes que esperan a ser atendidos
       for (i = 0; i < 10; i++)
 	{
-		if(tiempos[i]==0)
-		printf ("   --\t");    //Se imprime una linea, si la caja esta fuera de servicio.
-		else if (Size (&cajas[i]) >0)  
+		if(tiempos[i]==0) //Si la caja esta fuera de servicio, se imprime una linea.
+		printf ("   --\t");
+		else if (Size (&cajas[i]) >0)
 	    {
- printf ("    %d\t", Size (&cajas[i])-1);  //Se resta un 1, ya que en el tamaño de la fila de una caja, no se cuenta al cliente atendido en esa caja
+ printf ("    %d\t", Size (&cajas[i])-1); //Debemos considerar que no se cuenta al cliente atendido en el momento
 	    }
 	    else
-	  printf ("   %d\t", Size (&cajas[i]));  //... si no hay clientes atendidos/formados, se debe imprimir "0" y no "-1", esto viene de no contar al cliente atendido en una fila.
+	  printf ("   %d\t", Size (&cajas[i])); 
 	}
-      printf("\n\n\nLas personas llegan cada %d segundos", tiempos[10]);
+      printf("\n\n\nLas personas llegan cada %d segundos", tiempos[10]); //Imprimimos el tiempo de llegada de los clientes, propuesto al inicio del programa.
       printf ("\n  TIEMPO: %d segundos", tiempo_total);	//Imprime el tiempo que lleva la simulaciòn.
-      printf ("\t\t  Numero de clientes atendidos en total:\t  %d", atendidos);	//Imprime el numero total de clientes atendidos.
+      printf ("\t\t  Numero de clientes atendidos en total:\t  %d", atendidos);	//Imprime el nùmero total de clientes atendidos.
 }
